a96fbdf401883b3de94f6828b9f57e5232e16895
zchuwie <mayaloveptr.0108@gmail.com>
2024-10-31 16:27:20 +0800

    Add project files.

---
+++ Account/Login.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Account
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void toSignup_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var signup = new Signup();
+            signup.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Account/src/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.src
+{
+    public class Register
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        
+        public string login()
+        {
+            return null;
+        }
+    }
+}
